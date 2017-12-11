ac79a5e1248387895867cb7efac91b955baec1a0
Mateo Paredes <mateopase@gmail.com>
2017-12-11 16:12:53 -0500

    First commit, finished mini tutorial.

---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "github.com/mateopase/hellogo/stringutil"
+)
+
+func main() {
+    helloWorld := "Hello, world!"
+    fmt.Println(stringutil.Reverse(helloWorld))
+}
